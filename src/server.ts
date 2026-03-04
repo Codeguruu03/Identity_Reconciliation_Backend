a52c7cd9ffe113941e4912735f543cf9e4cd519f
Codeguruu <namananilgoyal@gmail.com>
2026-03-04 17:49:27 +0530

    feat: Initialize and start the Express server.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
